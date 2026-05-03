d0daa5cd1a0f33cccd6a9e81ea17c5fcdf115b09
Harun Reşid Ergen <83069560+HarunErgen@users.noreply.github.com>
2026-05-03 22:14:06 +0300

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def bt(nums, idx):
+            if idx == len(nums):
+                res.append(nums.copy())
+                return
+
+            seen = set()
+            for i in range(idx, len(nums)):
+                if nums[i] in seen:
+                    continue
+                seen.add(nums[i])
+                
+                nums[i], nums[idx] = nums[idx], nums[i]
+                bt(nums, idx + 1)
+                nums[i], nums[idx] = nums[idx], nums[i]
+
+        bt(nums, 0)
+        return res                    
+
+        
